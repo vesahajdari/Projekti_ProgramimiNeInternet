6af9e48990cb83900cb25982d7f9bc09b3de497d
MihrijeIbrahimi <75813395+MihrijeIbrahimi@users.noreply.github.com>
2021-05-21 23:35:00 +0200

    Add files via upload

---
+++ Food.php
@@ -0,0 +1,482 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+    <link rel="stylesheet" href="Food.css">
+    <script src="Food.js"></script>
+    <title>Food To Your Heart</title>
+</head>
+<body>
+    <header>
+          <div class="header">
+            <h1>FOOD</h1>
+            <p>Food To Your Heart</p>
+          </div>
+        
+    </header>
+
+    <main>
+       <div class="cont">
+        <div class="btopnav" id="bmyTopnav">
+          <a href="index.php " class="bactive"> <i class="fa fa-home icons" > </i> Home</a>
+          <div class="bdropdown">
+            <button class="bdropbtn"><i class="fa fa-cutlery icons" > </i> Food </i>
+              <i class="bfa bfa-caret-down"></i>
+            </button>
+            <div class="bdropdown-content">
+              <a href="#Meat">Meat</a>
+              <a href="#Vegetarian">Vegetarian</a>
+              <a href="#Vegan">Vegan</a>
+              
+            </div>
+          </div> 
+           <a href="Dessert.php"> <i class="fa fa-birthday-cake icons" > </i> Desserts   </a>
+          <a href="Drinks.php"> <i class="fa fa-glass icons" > </i> Drinks</a>
+          <a href="#signin"> <i class="fa fa-sign-in icons" ></i> Log In</a>
+          <a href="yourRecipe.php"> <i class="fa fa-caret-right icons" aria-hidden="true"></i></i> Your Recipe  </a>
+          <a href="javascript:void(0);" style="font-size:15px;" class="bicon" onclick="myFunction()">&#9776;</a>
+      </div>
+        
+
+    
+
+        <br>
+        <br>
+        <em>
+        <h2 id="Meat" style="text-align: center;"><i>MEAT </i></h2>
+        </em>
+    <div class="wrapper">
+        <div class="card">
+            <img src="/Foto/Food/BeefAndBroccoli.png" alt="BeefAndBroccoli">
+            <div class="info">
+                <h5>Beef and Broccoli</h5>
+                <p>Beef and Broccoli is a healthy Chinese food option
+                     with tender slices of meat and tender broccoli in
+                      less than 30 minutes and just a few ingredients!...
+                      <a href="BeefandBroccoli.php" target=_blank style="text-decoration: none; color: rgb(255, 15, 107);">Read more</a>
+                    </p>
+                        
+                        
+                
+                
+            </div>
+        </div>
+            <div class="card">
+                <img src="Foto/Food/RoastTurkey.jpg" alt="Roast Turkey">
+                <div class="info">
+                    <h5>Roast Turkey</h5>
+                    <p>Roast Turkey made with seasoned butter and broth
+                        is basted for over 4 hours for an incredibly
+                         tender meat and crispy skin is PERFECT for
+                          holiday dinner!
+                          <a href="RoastTurkey.php" target=_blank style="text-decoration: none; color: rgb(255, 15, 107);">Read more</a>
+                        </p>
+
+                    
+                    
+                    
+                </div>
+            </div>
+
+                <div class="card">
+                    <img src="Foto/Food/LimeShrimp.jpg" alt="Lime Shrimp">
+                    <div class="info">
+                        <h5>Grilled Cilantro Lime Shrimp</h5>
+                        <p>Grilled Cilantro Lime Shrimp is an easy recipe
+                             with a marinade of lime juice, olive oil, garlic
+                              and cilantro you can grill indoors or out in
+                               30 minutes!
+                               <a href="LimeShrimp.php" target=_blank style="text-decoration: none; color: rgb(255, 15, 107);">Read more</a>
+                             </p>
+                        
+        </div>
+    </div>
+    <div class="card">
+        <img src="FOTO/Food/LoMein.png" alt="Chicken Lo Mein">
+        <div class="info">
+            <h5>Chicken Lo Mein</h5>
+            <p>Chicken Lo Mein with chewy Chinese egg noodles, bean sprouts,
+                 chicken, bell peppers and carrots in under 30 minutes like
+                  your favorite Chinese takeout restaurant.
+                  <a href="LoMein.php" target=_blank style="text-decoration: none; color: rgb(255, 15, 107);">Read more</a>
+                </p>
+            
+            
+        </div>
+    </div>
+</div>
+                <!--wrapper-->
+                <br>
+<hr>
+                <em>
+                <h2 id="Vegetarian" style="text-align: center;"><i>VEGETARIAN</i></h2>
+                </em>
+                <br>
+                <br>
+        <div class="wrapper">
+            <div class="card">
+                <img src="FOTO/Food/PizzaDough.jpg" alt="Pizza Dough">
+                <div class="info">
+                    <h5>Easy Pizza Dough</h5>
+                    <p>Easy Pizza Dough made with flour, yeast,
+                         sugar, oil, and seasoning takes your
+                          homemade pizza to the next level, ready
+                           in 60 minutes!
+                           <a href="PizzaDough.php" target=_blank style="text-decoration: none; color: rgb(255, 15, 107);">Read more</a>
+                        </p>
+                   
+                </div>
+            </div>
+
+            <div class="card" >
+                <img src="FOTO/Food/GreenBean.jpg" alt="Stevestop Green Bean Casserole">
+                <div class="info">
+                    <h5>Stovetop Green Bean Casserole</h5>
+                    <p>Stovetop Green Bean Casserole recipe is an
+                         easy holiday side from scratch with green
+                          beans, and mushrooms in an easy garlic
+                           cream sauce with crispy onions
+                           <a href="GreenBean.php" target=_blank style="text-decoration: none; color: rgb(255, 15, 107);">Read more</a>
+                        </p>
+                    
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="FOTO/Food/VodkaSauce.jpg" alt="Vodka Sauce">
+                <div class="info">
+                    <h5>Vodka Sauce</h5>
+                    <p>This homemade Vodka Sauce recipe is so easy
+                         and full of flavor. Creamy Italian tomato
+                          sauce with heavy cream, vodka, canned tomatoes,
+                           and red pepper flakes.
+                           <a href="VodkaSauce.php" target=_blank style="text-decoration: none; color: rgb(255, 15, 107);">Read more</a>
+                    </p>
+                    
+                </div>
+            </div>
+            <div class="card">
+                <img src="FOTO/Food/FlorentinePasta.png" alt="Florentine Pasta">
+                <div class="info">
+                    <h5>Cheesy Florentine Pasta</h5>
+                    <p>Cheesy Florentine Pasta with an easy creamy Parmesan
+                         sauce, fresh spinach and roasted bell peppers. The
+                          perfect weeknight meal that's also great as a cold
+                           lunch option!
+                           <a href="FlorentinePasta.php" target=_blank style="text-decoration: none; color: rgb(255, 15, 107);">Read more</a>
+                        </p>
+                    
+                </div>
+            </div>
+        </div>
+
+        <!--wrapper-->
+        <br>
+        <hr>
+        <em>
+        <h2 id="Vegan" style="text-align: center;"><i>VEGAN</i></h2>
+        </em>
+        <br>
+        <br>
+        <div class="wrapper">
+            <div class="card">
+                <img src="FOTO/Food/RainbowVegetables.jpg" alt="Rainbow Roasted Vegetables">
+                <div class="info">
+                    <h5>Rainbow Roasted Vegetables</h5>
+                    <p>
+                        Rainbow Roasted Vegetables are the perfect
+                         way to enjoy eating healthy, colorful vegetables
+                          for adults and kids! Makes a perfect side for
+                           quick meals or dinner parties where you want
+                            to impress!
+                            <a href="RainbowVegetables.php" target=_blank style="text-decoration: none; color: rgb(255, 15, 107);">Read more</a>
+                        </p>
+                    
+                </div>
+            </div>
+
+
+            <div class="card">
+                <img src="FOTO/Food/MexicanSalad.jpg" alt="Mexican Corn Salad">
+                <div class="info">
+                    <h5>Mexican Corn Salad</h5>
+                    <p>
+                        Mexican corn salad is an easy no-cook side dish 
+                        for your summer bbq with corn, mayo, lime juice,
+                         chili powder, cilantro and cotija cheese that
+                          gives you elote flavors without the cob!
+                          <a href="MexicanSalad.php" target=_blank style="text-decoration: none; color: rgb(255, 15, 107);">Read more</a>
+                        </p>
+                    
+                </div>
+            </div>
+
+
+            <div class="card">
+                <img src="FOTO/Food/SauteedMushrooms.jpg" alt="Sauteed Mushrooms">
+                <div class="info">
+                    <h5>Sautéed Mushrooms</h5>
+                    <p>
+                        These Perfect Sautéed Mushrooms are an easy,
+                         delicious side dish, caramelized in butter with
+                          garlic; a great recipe for pasta, steak or pork
+                           chops!
+                           <a href="SauteedMushrooms.php" target=_blank style="text-decoration: none; color: rgb(255, 15, 107);">Read more</a>
+                        </p>
+                    
+                </div>
+            </div>
+            <div class="card">
+                <img src="FOTO/Food/GarlicNoodles.png" alt="P.F Chang's Garlic Noodles">
+                <div class="info">
+                    <h5>P.F. Chang’s Garlic Noodles</h5>
+                    <p>
+                        P.F. Chang's Garlic Noodles are a quick and easy Chinese
+                         noodle dish copycat dish you can make without the heavy
+                          flavors of a chow mein and as the perfect base for your
+                           favorite stir fry meals.
+                           <a href="GarlicNoodles.html" target=_blank style="text-decoration: none; color: rgb(255, 15, 107);">Read more</a>
+                        
+                    </p>
+                    
+                </div>
+            </div>
+        </div>     
+
+             
+        
+        <hr>
+       
+    
+    </div>
+        
+    </main>
+</body> 
+
+
+<footer class="footer">
+       
+    <div id="block_container">
+    <div id="div1" >
+      <ul class="nav">
+        <li><a href="https://twitter.com/" target="_blank" class="fa fa-twitter" id="signin"></a></li>
+        <li><a href="https://www.facebook.com/" target="_blank" class="fa fa-facebook"></a></li>
+        <li><a href="https://www.youtube.com/" target="_blank" class="fa fa-youtube"></a></li>  
+        <li><a href="https://www.instagram.com/" target="_blank" class="fa fa-instagram"></a></li>
+        <li><a href="https://www.pinterest.com/" target="_blank" class="fa fa-pinterest"></a></li> 
+      </ul>
+    </div>
+   
+    <?php
+   if(isset($_FILES['image'])){
+      $errors= array();
+      $file_name = $_FILES['image']['name'];
+      $file_size =$_FILES['image']['size'];
+      $file_tmp =$_FILES['image']['tmp_name'];
+      $file_type=$_FILES['image']['type'];
+      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+      
+      $extensions= array("jpeg","jpg","png");
+      
+      if(in_array($file_ext,$extensions)=== false){
+         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+      }
+      
+      if($file_size > 2097152){
+         $errors[]='File size must be excately 2 MB';
+      }
+      
+      if(empty($errors)==true){
+         move_uploaded_file($file_tmp,"images/".$file_name);
+         echo "Success";
+      }else{
+         print_r($errors);
+      }
+   }
+?>
+  
+  
+  
+  
+    <div>
+       
+        
+        <button onclick="document.getElementById('login-id01').style.display='block'" id="login1" style="width:auto;">Login</button>
+        
+        <div id="login-id01" class="login-modal">
+          
+          <form class="login-modal-content animate" action="/action_page.php" method="post" autocomplete="on">
+            <div class="login-imgcontainer">
+              <span onclick="document.getElementById('login-id01').style.display='none'" class="login-close" title="Close Modal">&times;</span>
+            </div>
+        
+            <div class="login-container">
+              <label for="uname" ><b>Username</b></label>
+              <input type="text" placeholder="Enter Username" name="uname" required>
+        
+              <label for="psw"><b>Password</b></label>
+              <input type="password" placeholder="Enter Password" name="psw" required>
+                
+              <button type="submit">Login</button>
+              <label>
+                <input type="checkbox" checked="checked" name="remember"> Remember me
+              </label>
+            </div>
+        
+            <div class="login-container" style="background-color:#f1f1f1">
+              <button type="button" onclick="document.getElementById('login-id01').style.display='none'" class="login-cancelbtn">Cancel</button>
+              <span class="login-psw">Forgot <a href="#">password?</a></span>
+            </div>
+          </form>
+        </div>
+        
+        <script>
+        // Get the modal
+        var modal = document.getElementById('login-id01');
+        
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+        </script>
+    </div>
+    <div>
+        
+        <button onclick="document.getElementById('signup-id01').style.display='block'" style="width:auto;">Sign Up</button>
+        
+        <div id="signup-id01" class="signup-modal">
+          <span onclick="document.getElementById('signup-id01').style.display='none'" class="signup-close" title="Close Modal">&times;</span>
+          <form class="signup-content" id="signupform" method="POST" action="" onsubmit="return validateSignUp();">
+          <div class="signup-container">
+            <h1>Sign Up</h1>
+            <p>Please fill in this form to create an account.</p>
+            <hr>
+            <label for="email"><b>Email</b></label>
+            <input id="femail" type="text" placeholder="Enter Email" name="email" required>
+      
+            <label for="psw"><b>Password</b></label>
+            <input type="password" id="fpassword" placeholder="Enter Password" name="psw" required>
+      
+            <label for="psw-repeat"><b>Repeat Password</b></label>
+            <input type="password" id="fpassword2" placeholder="Repeat Password" name="psw-repeat" onChange="return matchpass();" required>
+            
+            <label>
+              <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+            </label>
+      
+            <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+      
+            <div class="clearfix">
+              <button type="button" onclick="document.getElementById('signup-id01').style.display='none'" class="signup-cancelbtn">Cancel</button>
+              <button type="submit" class="signupbtn">Sign Up</button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <script>
+        function validateSignUp()
+      {  
+        var email=document.forms["signupform"]["femail"].value;  
+        var password=document.forms["signupform"]["fpassword"].value; 
+        var regex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/i; 
+          
+        if (email==null || email=="" || regex.test(email) == false)
+        {  
+          alert("Email must be filled out ");  
+          return false;  
+        }
+        else if(password.length<6)
+        {  
+          alert("Password must be at least 6 characters long.");  
+          return false;  
+          }  
+      } 
+    
+        function matchpass()
+      {  
+        var firstpassword=document.forms["signupform"]["fpassword"].value;  
+        var secondpassword=document.forms["signupform"]["fpassword2"].value;  
+          
+        if(firstpassword==secondpassword){  
+        return true;  
+        }  
+        else{  
+        alert("Passwords must be the same!");  
+        return false;  
+        }  
+      }  
+      </script>
+        
+        <script>
+        // Get the modal
+        var modal = document.getElementById('signup-id01');
+        
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+        }
+        window.onscroll = function() {myFunction()};
+  
+      var header = document.getElementById("bmyTopnav");
+      var sticky = header.offsetTop;
+  
+      function myFunction() {
+        if (window.pageYOffset > sticky) {
+          header.classList.add("sticky");
+        } else {
+          header.classList.remove("sticky");
+        }
+      }
+        </script>
+    </div>
+  </div>
+  
+  <div>
+  <table>
+  <tr>
+  <th><a href="Food.php">FOOD</a></th>
+    <th><a href="Dessert.php">DESSERT</a></th>
+    <th><a href="Drinks.php">DRINKS</a></th>
+  </tr>
+  <tr>
+    <td>Meat</td>
+    <td>Baking</td>
+    <td>Hot</td>
+  </tr>
+  <tr>
+    <td>Vegetarian </td>
+    <td>Nonbaking</td>
+    <td>Cold</td>
+  </tr>
+  <tr>
+    <td>Vegan</td>
+    <td>Frozen</td>
+    <td>Alcoholic</td>
+  </tr>
+  <tr>
+    <td></td>
+    <td>Pastries</td>
+    <td>Non-Alcoholic</td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td>New Drinks</td>
+  </tr>
+  </table>
+  
+  </div>
+
+
+   
+  </footer>
+  
+  
+</body>
+</html>
